e97d236194ba267b4125ad5cdb3ba8a00035567e
Ankit Kumar Chaudhary <ankitkchy101226@gmail.com>
2022-05-08 19:49:15 +0530

    Dom_8thMay

---
+++ Complete Web Dev/dom/index.js
@@ -0,0 +1,4 @@
+// Question:- What is dom?
+//Ans:- dom basically catalogs the web page into individual objects that we can select and manipulate
+document.querySelector("li a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
